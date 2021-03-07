cd607e077cfce3d736e13d5fc2bde73db218c1c6
Sergey <xxsmotur@gmail.com>
2021-03-07 17:37:27 +0500

    Add basic tests for getFieldsFromStruct

--- reflect_test.go
+++ reflect_test.go
@@ -2,11 +2,15 @@ package ormatic
 
 import (
 	"testing"
-	
-	"github.com/saromanov/ormatic/models"
+
 	"github.com/stretchr/testify/assert"
 )
 
-func testGetFieldsFromStruct(t *testing.T) {
-	
+func TestGetFieldsFromStruct(t *testing.T) {
+	_, err := getFieldsFromStruct(nil)
+	assert.Error(t, errNoStruct, err)
+	_, err = getFieldsFromStruct(4)
+	assert.Error(t, errNoStruct, err)
+	_, err = getFieldsFromStruct([]string{})
+	assert.Error(t, errNoStruct, err)
 }
