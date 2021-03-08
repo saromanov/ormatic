e06777e50a4e9275e5a41ff677be2b46a7d4ed03
Sergey <xxsmotur@gmail.com>
2021-03-08 12:05:41 +0500

    Implementation of save method

--- ormatic.go
+++ ormatic.go
@@ -3,6 +3,7 @@ package ormatic
 import (
 	"database/sql"
 	"github.com/pkg/errors"
+	"github.com/saromanov/ormatic/generate"
 )
 // Ormatic defines the main structure
 type Ormatic struct {
@@ -21,5 +22,18 @@ func New(db *sql.DB) (*Ormatic, error) {
 
 // Save provides saving of the data
 func (o *Ormatic) Save(d interface{}) error {
+	return o.save(d)
+}
+
+func (o *Ormatic) save(d interface{}) error {
+	fields, err := getFieldsFromStruct(d)
+	if err != nil {
+	  return errors.Wrap(err, "unable to get fields from the struct")
+	}
+	stat, err := generate.Insert("", fields)
+	if err != nil {
+		return errors.Wrap(err, "unable to generate statement")
+	}
+	fmt.Println(stat)
 	return nil
 }
