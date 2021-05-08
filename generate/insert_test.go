84027e16806027d5ce1fce8b354766bacb9a971b
Sergey <xxsmotur@gmail.com>
2021-05-08 12:57:13 +0500

    Update tests for insert

--- generate/insert_test.go
+++ generate/insert_test.go
@@ -7,26 +7,32 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 func TestInsertBasic(t *testing.T) {
-	_, _, err := Insert("", []models.Pair{})
+	_, _, err := Insert(models.Insert{})
 	assert.Error(t, errNoTableName, err)
 
-	data, values, err := Insert("test", []models.Pair{models.Pair{
-		Key: "key",
-		Value: "value",
-	}})
+	data, values, err := Insert(models.Insert{
+		TableName: "test",
+		Pairs: []models.Pair{models.Pair{
+			Key: "key",
+			Value: "value",
+		}},
+	})
 	assert.NoError(t, err)
 	assert.Equal(t, data, "INSERT INTO test (key) VALUES ($1)")
 	assert.Equal(t, "value", values[0].(string))
 
-	data, values, err = Insert("test2", []models.Pair{models.Pair{
-		Key: "key",
-		Value: "value",
-	},
-
-	models.Pair{
-		Key:"foo",
-		Value:"bar",
-	}})
+	data, values, err = Insert(models.Insert{
+		TableName: "test2",
+		Pairs: []models.Pair{models.Pair{
+			Key: "key",
+			Value: "value",
+		},
+		models.Pair{
+			Key:"foo",
+			Value:"bar",
+		},
+	   },
+	})
 	assert.NoError(t, err)
 	assert.Equal(t, data, "INSERT INTO test2 (key,foo) VALUES ($1,$2)")
 	assert.Equal(t, "value", values[0].(string))
